084c36d391748ed966e31a4620ed0ded392a2cf4
Ofir Cooper <ofir.cooper@gmail.com>
2015-12-02 00:31:36 +0200

    Added exit door, and level loading mechanism

---
+++ Assets/ExitDoor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitDoor : MonoBehaviour {
+
+    private bool red = false;
+    private bool blue = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "RedPlayer")
+            red = true;
+        if (collider.gameObject.name == "BluePlayer")
+            blue = true;
+        if (red && blue)
+        {
+            globals.current_level++;
+            if (globals.current_level < globals.levels.Length)
+            {
+                Application.LoadLevel(globals.levels[globals.current_level]);
+            }
+            else
+            {
+                // show game over screen
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "RedPlayer")
+            red = false;
+        if (collider.gameObject.name == "BluePlayer")
+            blue = false;
+    }
+}
---
+++ Assets/globals.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class globals : MonoBehaviour {
+
+    public static int current_level;
+    public static string[] levels = { "testing_ground" };
+
+	// Use this for initialization
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+	void Start () {
+        current_level = 1;
+        Application.LoadLevel(levels[current_level-1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
