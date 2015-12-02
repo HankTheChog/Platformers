0dc526d7efc844ce3a0f75af1632967ca08fa65c
Ofir Cooper <ofir.cooper@gmail.com>
2015-12-02 22:56:36 +0200

    Added Globals; persists across levels

---
+++ Assets/Globals.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour {
+
+    public static int current_level = 1;
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(transform.gameObject);
+        Application.LoadLevel(current_level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
