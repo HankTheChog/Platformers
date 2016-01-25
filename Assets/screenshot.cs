8a6443a297e9b33cd8860f6ae4acb4b7f456fa3c
Ofir Cooper <ofir.cooper@gmail.com>
2016-01-25 23:27:03 +0200

    Added graphics assets; adjusted cameras; put in background

---
+++ Assets/screenshot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class screenshot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.K))
+        {
+            Application.CaptureScreenshot("Screenshot_level_" + EditorSceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
